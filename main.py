b3149b5ea34a686fa4a1a855bd6278a5d5bcfc14
LikeToAccess <375916737591675+LikeToAccess@users.noreply.github.com>
2022-04-25 10:29:44 -0500

    Imported base from LikeToAccess/LMB

---
+++ main.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+# filename          : main.py
+# description       : Respond to GET requests from websites with MP4 links
+# author            : Ian Ault
+# email             : liketoaccess@protonmail.com
+# date              : 04-25-2022
+# version           : v1.0
+# usage             : python main.py
+# notes             :
+# license           : MIT
+# py version        : 3.10.2 (must run on 3.6 or higher)
+#==============================================================================
+import os
+import time
+from selenium import webdriver
+from selenium.common.exceptions import *
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+from tqdm import tqdm
+from settings import *
+
+
+class Scraper:
+	def __init__(self):
+		options = Options()
+		user_data_dir = os.path.abspath("selenium_data")
+		options.add_argument(f"user-data-dir={user_data_dir}")
+		options.add_argument("log-level=3")
+		if HEADLESS:
+			options.add_argument("--headless")
+			options.add_argument("--disable-gpu")
+		self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+		# self.driver.minimize()
+		print("Init finished")
+
+	def wait_until_element(self, selector, locator, timeout=10):
+		wait = WebDriverWait(self.driver, timeout)
+		element = wait.until(
+			EC.presence_of_element_located(
+				(
+					selector, locator
+				)
+			)
+		)
+		return element
+
+	def wait_until_elements(self, selector, locator, timeout=10):
+		wait = WebDriverWait(self.driver, timeout)
+		elements = wait.until(
+			EC.presence_of_all_elements_located(
+				(
+					selector, locator
+				)
+			)
+		)
+		return elements
+
+	def find_element(self, selector, sequence):
+		return self.driver.find_element(selector, sequence)
+
+	def find_elements(self, selector, sequence):
+		return self.driver.find_elements(selector, sequence)
+
+	def wait_until_element_by_xpath(self, sequence, timeout=10):
+		return self.wait_until_element(By.XPATH, sequence, timeout=timeout)
+
+	def wait_until_elements_by_xpath(self, sequence, timeout=10):
+		return self.wait_until_elements(By.XPATH, sequence, timeout=timeout)
+
+	def find_element_by_xpath(self, sequence):
+		return self.find_element(By.XPATH, sequence)
+
+	def find_elements_by_xpath(self, sequence):
+		return self.find_elements(By.XPATH, sequence)
+
+	def open_link(self, url):
+		self.driver.get(url)
+
+	def current_url(self):
+		return self.driver.current_url
+
+	def close(self):
+		self.driver.close()
+
+	def refresh(self):
+		self.driver.refresh()
+
+	def run(self):
+		self.open_link("https://gomovies-online.cam/")
+		print("Press any key to continue...")
+		os.system("read -n1 -r")
+
+
+def main():
+	scraper = Scraper()
+	scraper.run()
+	# scraper.close()
+
+
+if __name__ == "__main__":
+	main()
---
+++ settings.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# filename          : settings.py
+# description       : Different options for main.py
+# author            : Ian Ault
+# email             : service@nanosystems1.com
+# date              : 04-11-2022
+# version           : v1.0
+# usage             : python main.py
+# notes             : This file should not be run directly
+# license           : MIT
+# py version        : 3.10.2
+#==============================================================================
+# Sets the browser option "--headless", this will prevent the browser from
+# opening a GUI window.
+# Because of this, the "--disable-gpu" flag is also enabled when HEADLESS is
+# set to True.
+# The default value is True.
+HEADLESS = False
