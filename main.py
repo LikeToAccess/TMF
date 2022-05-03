154532d79439d872dd78f17e85206c664fd08aa7
LikeToAccess <375916737591675+LikeToAccess@users.noreply.github.com>
2022-05-03 07:36:33 -0500

    Added code to fully search and record info

--- main.py
+++ main.py
@@ -12,6 +12,7 @@
 #==============================================================================
 import os
 import time
+import json
 import platform
 from selenium import webdriver
 from selenium.common.exceptions import *
@@ -21,11 +22,51 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-from tqdm import tqdm
+# from tqdm import tqdm
 from settings import *
 
 
 OS = platform.system()
+
+
+def remove_file(filename):
+	try:
+		os.remove(filename)
+		return True
+	except OSError:
+		return False
+
+def read_file(filename, encoding="utf8"):
+	if not os.path.exists(filename): return filename
+	with open(filename, "r", encoding=encoding) as file:
+		data = file.read()
+
+	return data
+
+def write_file(filename, data, encoding="utf8"):
+	with open(filename, "w", encoding=encoding) as file:
+		file.write(data)
+
+def read_json_file(filename, encoding="utf8"):
+	if not os.path.exists(filename): return []
+	with open(filename, "r", encoding=encoding) as file:
+		data = json.load(file)
+
+	return data
+
+def write_json_file(filename, data, encoding="utf8"):
+	with open(filename, "w", encoding=encoding) as file:
+		json.dump(data, file, indent=4, sort_keys=True)
+
+	return pretty_print_json(data)
+
+def append_json_file(filename, data, encoding="utf8"):
+	existing_data = read_json_file(filename, encoding=encoding)
+	existing_data.append(data)
+	write_json_file(filename, existing_data, encoding=encoding)
+
+def pretty_print_json(data):
+	return json.dumps(data, indent=4, sort_keys=True)
 
 
 class Scraper:
@@ -94,15 +135,96 @@ class Scraper:
 	def refresh(self):
 		self.driver.refresh()
 
+	def find_subtitles_source(self):
+		sequence = "/html/body/main/div/div/section/div[3]/div/script[2]/text()"
+		element = self.find_element_by_xpath(sequence)
+		print(element.text)
+		# source = element.get_attribute("src")
+		#/html/body/main/div/div/section/div[3]/div/script[2]/text()
+
+	def find_video_source(self):
+		pass
+		#//*[@id="_skqeqEJBSrS"]/div[2]/video
+
+	def current_page_is_404(self):
+		try:
+			if self.find_element_by_xpath("//*[@id='_sKnoHaKJFse']/div[1]/h1/p[1]").text == "404":
+				return True
+		except NoSuchElementException:
+			pass
+
+		return False
+
+	def search_by_title(self, search_term):
+		self.open_link(f"https://gomovies-online.cam/search/{search_term}")
+
+		if self.current_page_is_404():
+			print("ERROR: Page error 404!\n")
+			return self.search_by_title(search_term)
+
+		results_data = []
+		results = self.find_elements_by_xpath("//*[@class='item_hd']") + \
+				  self.find_elements_by_xpath("//*[@class='item_sd']") + \
+				  self.find_elements_by_xpath("//*[@class='item_cam']") + \
+				  self.find_elements_by_xpath("//*[@class='item_series']")
+		for result in results:
+			# Title
+			title = result.text
+			# Poster
+			poster_url = result.find_element(by=By.XPATH, value="div/div/img").get_attribute("src")
+			# URL
+			url = result.get_attribute("href")
+			# Other Data
+			_data_element = result.find_element(by=By.XPATH, value="..")
+			additional_data = {
+				"title":               _data_element.get_attribute("data-filmname"),
+				"release_year":        _data_element.get_attribute("data-year"),
+				"imdb_score":          _data_element.get_attribute("data-imdb"),
+				"duration":            _data_element.get_attribute("data-duration"),
+				"release_country":     _data_element.get_attribute("data-country"),
+				"genre":               _data_element.get_attribute("data-genre"),
+				"description_preview": _data_element.get_attribute("data-descript"),  # Limit is 151 characters
+				"star_prefix":         _data_element.get_attribute("data-star_prefix"),
+				"key":                 _data_element.get_attribute("data-key"),
+				"quality_tag":         _data_element.get_attribute("data-quality"),
+				"user_rating":         _data_element.get_attribute("data-rating"),
+			}
+
+			additional_data["description_preview"] = \
+				additional_data["description_preview"].strip(". ").rsplit(" ", 1)[0].strip(".") + "..."
+			additional_data["quality_tag"] = \
+				additional_data["quality_tag"].replace("itemAbsolute_", "").upper()
+
+			results_data.append(
+				{
+					"title":           title,
+					"poster_url":      poster_url,
+					"url":             url,
+					"additional_data": additional_data,
+				}
+			)
+
+			if title != additional_data["title"]:
+				print("WARNING: Titles do not match!")
+				print(f"\tGot:      '{additional_data['title']}'")
+				print(f"\tExpected: '{title}'\n")
+
+		return results_data
+
 	def run(self):
 		print(f"Completed init in {round(time.time()-self.begin_init_timestamp,2)}s.\n")
-		search_term = input("Enter a movie name to search for:\n> ")
-		self.open_link(f"https://gomovies-online.cam/search/{search_term}")
-		results = self.find_elements_by_xpath("//*[@class='item_hd']")
-		for result in results:
-			print(result.text)
 
-		wait_for_input()
+		while True:
+			search_term = input("Enter a movie name to search for:\n> ")
+
+			print("Waiting for search results...")
+			begin_search_timestamp = time.time()
+			results = self.search_by_title(search_term)
+			print(f"Completed search in {round(time.time()-begin_search_timestamp,2)}s,", end=" ")
+			print(f"found {len(results)} {'result' if len(results) == 1 else 'results'}.\n")
+			print(pretty_print_json(results))
+
+			wait_for_input()
 
 
 def wait_for_input():
