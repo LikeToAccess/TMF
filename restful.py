4f4a54c463fd03aef6c8547ac7ce50298a6e47fc
LikeToAccess <37591675+LikeToAccess@users.noreply.github.com>
2022-05-30 02:58:04 -0500

    Edited flask API

--- restful.py
+++ restful.py
@@ -10,7 +10,7 @@
 # license           : MIT
 # py version        : 3.10.2 (must run on 3.6 or higher)
 #==============================================================================
-from contextlib import closing
+# from contextlib import closing
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from waitress import serve
@@ -20,24 +20,24 @@ app = Flask(__name__)
 api = Api(app)
 
 
-class Users(Resource):
+class Plex(Resource):
 	def get(self):
+		# Gets search results from a search term
 		parser = reqparse.RequestParser()
-		parser.add_argument("email", required=False, type=str, location="args")
-		parser.add_argument("name", required=False, type=str, location="args")
+		parser.add_argument("search_term", required=True, type=str, location="args")
+		# parser.add_argument("name", required=False, type=str, location="args")
 		args = parser.parse_args()
 		if args:
-			return {"message": "Movie link"}, 200
+			return {"message": f"Searching for Movie: '{args['search_term']}'"}, 200
+		return {"message": "Bad request"}, 400
 
 	def post(self):
+		# Adds media to the server
 		parser = reqparse.RequestParser()
-		parser.add_argument("name", required=True, type=str, location="args")
-		parser.add_argument("email", required=True, type=str, location="args")
-		parser.add_argument("profile_picture", required=False, type=str, location="args")
+		parser.add_argument("data", required=True, type=str, location="args")
 		args = parser.parse_args()
 
-		print("Succesful 200 in POST request: New user created")
-		return {"message": "New user created"}, 200
+		return {"message": f"Movie added, '{args['data']}'"}, 200
 
 class Sample(Resource):
 	def get(self):
@@ -49,7 +49,7 @@ class Sample(Resource):
 
 def main():
 	# plexserver.ga:8080/sample
-	api.add_resource(Users, "/users")
+	api.add_resource(Plex, "/plex")
 	api.add_resource(Sample, "/sample")
 	serve(app, host="0.0.0.0", port=8080)
 	# app.run()
